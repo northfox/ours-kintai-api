ff2e370528d5cd1f242fd2b12621646f1f5689a2
northfox <vulpes.engineer@gmail.com>
2019-12-20 08:50:30 +0900

    Init by SpringInitializr

---
+++ src/main/java/com/github/northfox/ours/kintai/kintai/api/Application.java
@@ -0,0 +1,13 @@
+package com.github.northfox.ours.kintai.kintai.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
